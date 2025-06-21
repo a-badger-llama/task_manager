ba81e8409609f5f18a60c33278b182cf12886455
jonathanfox <jonathan.fox1991@gmail.com>
2025-06-21 14:30:26 -0600

    Restrict `tasks` routes and add comprehensive controller tests
    - disable `new`, `show`, and `edit` routes for `tasks` since they are not used at the moment and remove actions from the controller
    - add thorough controller tests for all `tasks` CRUD actions
    - include `mocha` gem for mocking and stubbing in tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,68 +1,57 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:update, :destroy]
 
-  # GET /tasks
   def index
-    @tasks = Task.incomplete.position.order(created_at: :desc)
+    @tasks           = Task.incomplete.position.order(created_at: :desc)
     @completed_tasks = Task.complete.position.order(created_at: :desc)
   end
 
-  # GET /tasks/1 or /tasks/1.json
-  def show
-    respond_to do |format|
-      format.turbo_stream
-    end
-  end
+  # def show
+  # end
+  #
+  # def new
+  #   @task = Task.new
+  # end
+  #
+  # def edit
+  # end
 
-  # GET /tasks/new
-  def new
-    @task = Task.new
-
-    respond_to do |format|
-    end
-  end
-
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params.merge(user: current_user))
 
     respond_to do |format|
-      if @task.save
-        @dom_id = params[:dom_id]
+      @dom_id = params[:dom_id]
 
+      if @task.save
         format.turbo_stream
-        format.json { head :no_content }
       else
-        format.json { render json: { errors: @task.errors }, status: :unprocessable_entity }
+        format.turbo_stream do
+          render status: :bad_request, json: { errors: @task.errors.full_messages }
+        end
       end
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
-        @dom_id = params[:dom_id]
+        @dom_id               = params[:dom_id]
         @completed_tasks_size = Task.complete.size
 
         format.turbo_stream
-        format.json { head :no_content }
       else
-        format.json { render json: { errors: @task.errors }, status: :unprocessable_entity }
+        format.turbo_stream do
+          render status: :bad_request, json: { errors: @task.errors.full_messages }
+        end
       end
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
   def destroy
     @task.destroy!
+
     respond_to do |format|
       format.turbo_stream
-      format.json { head :no_content }
     end
   end
 
@@ -76,15 +65,13 @@ class TasksController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_task
     @task = Task.find(params.expect(:id))
   rescue ActiveRecord::RecordNotFound
     redirect_to tasks_path
   end
 
-  # Only allow a list of trusted parameters through.
   def task_params
-    params.expect(task: [ :title, :description, :due_date, :due_time, :completed ])
+    params.expect(task: [:title, :description, :due_date, :due_time, :completed])
   end
 end
